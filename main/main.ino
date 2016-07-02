d1a6e3f4a259851606a8bbd8f675276300a7c017
Dolan Miu <dolan_miu@hotmail.com>
2016-07-02 18:57:12 +0100

    added adafruit version of the project

--- main/main.ino
+++ main/main.ino
@@ -3,8 +3,7 @@
 int motorRelayPin = 3;
 int lightSensorPin = 0;
 
-
-const int stepsPerRevolution = 200; 
+const int stepsPerRevolution = 200;
 //Stepper myStepper(stepsPerRevolution, 8, 9);
 
 void setup() {
@@ -12,29 +11,31 @@ void setup() {
   Serial.begin(9600);
   //analogReference(EXTERNAL);
   pinMode(motorRelayPin, OUTPUT);
-  
+
   //myStepper.setSpeed(60);
 
-  pinMode(8, OUTPUT);     
+  pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
+  digitalWrite(motorRelayPin, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   /*digitalWrite(motorRelayPin, HIGH);
-  Serial.println("Motor power turned on");
-  delay(1000);
-  digitalWrite(motorRelayPin, LOW);
-  delay(1000);*/
+    Serial.println("Motor power turned on");
+    delay(1000);
+    digitalWrite(motorRelayPin, LOW);
+    delay(1000);*/
+  digitalWrite(motorRelayPin, HIGH);
   int lightValue = analogRead(lightSensorPin);
   //Serial.println(lightValue);
   if (lightValue < 20) {
-    digitalWrite(motorRelayPin, HIGH);
-    //Serial.println("Motor power turned on");
+    //digitalWrite(motorRelayPin, HIGH);
+    Serial.println("Motor power turned on");
   } else {
-    digitalWrite(motorRelayPin, LOW);
+    //digitalWrite(motorRelayPin, LOW);
     //Serial.println("Motor power turned off");
   }
 
@@ -47,7 +48,9 @@ void loop() {
   //delay(500);
 
   digitalWrite(9, HIGH);
-  delay(1);          
-  digitalWrite(9, LOW); 
-  delay(1); 
+  Serial.println("turning");
+  delay(1000);
+  Serial.println("not turning");
+  digitalWrite(9, LOW);
+  delay(1000);
 }
